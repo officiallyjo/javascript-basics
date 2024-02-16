f67e71c285a3169071439d01e6b1f93126aabff4
officiallyjo <josie9447@gmail.com>
2024-02-16 10:23:22 +0100

    arrays

---
+++ 08-arrays.js
@@ -0,0 +1,15 @@
+var students =["ali","bayo","yemi","temi"];
+//console.log(students);
+
+
+//new array keyword changes any data type into an array
+var states = new Array("abuja","sokoto","lagos","abia");
+//removes the last item in an array
+console.log(states.pop());
+//checks the lenght of the array
+console.log(states.length);
+//prints the 1st item in the array
+console.log(states[0]);
+
+// var new_states = states.pop();
+// console.log(new_states);
