be42b73e4eb88bc002a6c2acf34e906908427c27
officiallyjo <josie9447@gmail.com>
2024-02-17 17:52:16 +0100

    while loop

---
+++ 11-while-loop.js
@@ -0,0 +1,11 @@
+// while(condition){
+//     loop Body;
+//     i++;
+// }
+var students =["ali","bayo","yemi","temi"];
+
+var i = 0 ;
+while (i <= students.length){
+     console.log(students[i])
+     i++;
+}
