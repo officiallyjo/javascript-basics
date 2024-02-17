23ebe148d358d5cf2744de27556cfa59aff28bc6
officiallyjo <josie9447@gmail.com>
2024-02-17 17:32:46 +0100

    for loop

---
+++ 10-for-loop.js
@@ -0,0 +1,13 @@
+// for(initialization; condition ; increment/decrement){
+//     for loop body
+// }
+
+// for (var i = 0; i <= 10 ; i++){
+//     console.log(i)
+// }
+
+var students =["ali","bayo","yemi","temi"];
+
+for(i = 0; i<= students.length; i++){
+    console.log(students[i]);
+}
